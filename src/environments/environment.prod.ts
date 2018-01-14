6344298e429cbced79d73d78f2a6f5a3d65e67ba
Quentin Monmert <qmonmert@qmonmert.home>
2018-01-14 19:34:59 +0100

    Deploy on firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDncOWBNNXD5rCCF5n8QO-TaRLo94RLFpY",
+    authDomain: "angular-v510.firebaseapp.com",
+    databaseURL: "https://angular-v510.firebaseio.com",
+    projectId: "angular-v510",
+    storageBucket: "",
+    messagingSenderId: "1004771182246"
+  }
 };
